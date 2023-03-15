86cff6434035aadf0dc36351a662c811ff252738
Francis Maina <sebastianfrancix@gmail.com>
2023-03-15 08:51:51 +0300

    added state to handle which form to display to the user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
 const App = () => {
+  // Set state to handle which form to display
+  const [currentForm, setCurrentForm] = useState["Login"];
+
   return (
-    <div>
-      <Login />
+    <div className="app">
+      currentForm ==="Login" ? <Login /> : <Signup />
     </div>
   );
 };
