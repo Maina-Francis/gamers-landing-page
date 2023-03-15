e3904453753d78aaeaacf93741127403133c7027
Francis Maina <sebastianfrancix@gmail.com>
2023-03-15 06:24:23 +0300

    added Login and Signup component boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 export default App;
